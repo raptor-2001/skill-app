d99182afda60c9ef9e0a09c8a9bc5e6ac1836f0f
pratapshingane18 <sushrutrao465@gmail.com>
2023-02-24 23:27:45 +0530

    created separate brach for scholarship and added componenet struct

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from '../partials/Header';
+// import { NavBar } from '../NavBar';
 import HeroHome from '../partials/HeroHome';
 import FeaturesHome from '../partials/Features';
 import FeaturesBlocks from '../partials/FeaturesBlocks';
