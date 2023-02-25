cf18075e094c3f1bdfaa33005fae2a1d10177060
pratapshingane18 <sushrutrao465@gmail.com>
2023-02-25 16:01:35 +0530

    mentor page compleing

--- client/src/pages/Mentors.jsx
+++ client/src/pages/Mentors.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Footer from '../partials/Footer';
+import GetMentor from '../partials/mentor/Get-Mentor';
+import Navbar from '../partials/Navbar';
+
 
 const Mentors = () => {
   return (
-    <div>Mentors</div>
+    <div>
+
+      <Navbar />
+
+
+    <GetMentor />
+      {/* <Footer /> */}
+    </div>
   )
 }
 
