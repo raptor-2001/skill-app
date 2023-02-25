37845a3e1dca34354b4e602ff80d6f2829da2ce1
raptor-2001 <pratham007yadav@gmail.com>
2023-02-25 10:54:36 +0530

    navbar-fixed

--- client/src/partials/Navbar.jsx
+++ client/src/partials/Navbar.jsx
@@ -62,15 +62,7 @@ const Navbar = () => {
                       </Link>
                 </li>
 
-                <li>  
-                      <NavLink
-                        className="text-gray-500 transition hover:text-gray-500/75"
-                        activeClassName="text-teal-600"
-                        exact
-                        to="/Get-Tution">
-                        Get Tution
-                      </NavLink>
-                </li>
+               
 
                 
               </ul>
